67284c845383570c49136c3cf7f42a68e4441d1d
DharcKnati <rafaeajas@gmail.com>
2026-04-08 13:17:05 -0300

    Add files via upload

---
+++ menu01.lua
@@ -0,0 +1,14 @@
+scn = {}
+
+function scn:load()
+end
+
+function scn:update()
+end
+
+function scn:draw()
+  createButton("Button 01", 50, 50, 2, 0.8)
+  createButton("Second Button", 100, 80, 3, 3)
+end
+
+return scn
